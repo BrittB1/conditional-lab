2b0ab6e722dc5f7058357d1d9b095b322e64cf9d
Brittany Buchanan <157894600+BrittB1@users.noreply.github.com>
2026-07-09 08:51:44 -0400

    Solved  parts 1 thru 3

--- conditional.js
+++ conditional.js
@@ -18,3 +18,53 @@ let plantCount = startingPlants * (2** week)
 
 // AREA / Max capacity of the garden
 let maxCapacity = area / minSpacePerPlant;
+
+// ======================================
+//                 PART 1
+// ======================================
+const week1 = 1;
+const firstPlantCount = startingPlants * (2 ** week1);
+
+if (firstPlantCount > maxCapacity * 0.8) {
+    console.log("Week 1: " + firstPlantCount + " plants should be pruned.");
+
+} else if (firstPlantCount >= maxCapacity * 0.5) {
+    console.log("Week 1: " + firstPlantCount + " plants should be monitored.");
+
+} else {
+    console.log("Week 1: " + firstPlantCount + " plants should be planted.");
+}
+
+// ======================================
+//                 PART 2
+// ======================================
+const bigStart = 100;
+const bigWeeks = 10;
+
+const bigPlantCount = bigStart * (2 ** bigWeeks);
+const spaceNeeded = bigPlantCount * minSpacePerPlant;
+const additionalSpace = spaceNeeded - area;
+
+const newRadius = Math.sqrt(spaceNeeded / PI);
+
+console.log("Plants after 10 weeks: " + bigPlantCount);
+console.log("Space needed: " + spaceNeeded + " m2");
+console.log("Additional space: " + additionalSpace + " m2");
+console.log("New radius: " + newRadius + " m");
+
+// ======================================
+//                 PART 3
+// ======================================
+try {
+  const stubbornPlants = 100;
+  const spaceRequired = stubbornPlants * minSpacePerPlant;
+
+  if (spaceRequired > area) {
+    throw new Error("Too cramped!! Need " + spaceRequired + " m2 but only have " + area + "m2.");
+  }
+
+  console.log("The plants have enough space now and are thriviiiiing.");
+
+} catch (err) {
+  console.log("ERROR: " + err.message);
+}
